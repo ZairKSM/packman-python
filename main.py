488a97d2780e1b615ca4525ee3c63a9cadb33ea7
Yanis-Mansouri <yanis.mansouri333@gmail.com>
2021-03-30 09:13:21 +0200

    tkinter base

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import pathlib
+from tkinter import *
+from tkinter import messagebox
+import tkinter.font as tkFont
+from PIL import Image, ImageTk
+from random import randint
+from time import sleep
+
+mywindow = Tk()
+
+
+
+
+mywindow.resizable(False,False)
+mywindow.wm_title("Pokémons")
+mywindow.geometry("900x900")
+mywindow.configure(bg="white")
+canvas = Canvas(mywindow,width=1200, height=800, background='white')
+
